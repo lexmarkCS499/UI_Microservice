bb2e4d39387f476673f44c29e36d20e01f46c33b
Kamal Ghimire <kamalghimire121@gmail.com>
2024-11-07 22:36:23 +0000

    Updated the UI

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Enter a name maybe below to see how many times it's been entered! this should make a new pod testing some change
+          Enter a name maybe below to see how many times it's been entered!
         </p>
           <NameForm />
           <MyButton />
